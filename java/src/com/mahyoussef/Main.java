f05956c913b733c6e1aa9ed2f670f6bef59d9aa5
Mahmoud Youssef <mahmoud.youssef@booking.com>
2023-05-10 00:18:39 +0200

    adding hello world

--- java/src/com/mahyoussef/Main.java
+++ java/src/com/mahyoussef/Main.java
@@ -3,6 +3,11 @@ package com.mahyoussef;
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
+	    // write your code here\
+        System.out.println(printGreeting());
+    }
+
+    private static String printGreeting() {
+        return "Hello to The Refactoring Journey!";
     }
 }
